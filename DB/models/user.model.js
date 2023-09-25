9013abfc287fd93ad9c6b3d9d13832ef3f73e9b4
Fatma Amr <fatmaamr@Fatmas-MacBook-Pro.local>
2023-09-25 23:40:27 +0300

    initial commit

---
+++ DB/models/user.model.js
@@ -0,0 +1,65 @@
+import mongoose,  { Schema, model } from "mongoose";
+
+//schema
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    isConfirmed: {
+      type: Boolean,
+      default: false,
+    },
+    forgetCode: String,
+    activationCode: String,
+    profileImage: {
+      url: {
+        type: String,
+        default:
+          "https://res.cloudinary.com/dc4zgmrmf/image/upload/v1692197970/ecommerce%20dafaults/user/blank-profile-picture-973460_1280_dogmy1.webp",
+      },
+      id: {
+        type: String,
+        default:
+          "ecommerce%20dafaults/user/blank-profile-picture-973460_1280_dogmy1",
+      },
+    },
+    coverImages: [
+      {
+        url: { type: String, required: true },
+        id: { type: String, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// model
+export const User = mongoose.models.User || model("User", userSchema);
---
+++ src/modules/auth/auth.validation.js
@@ -0,0 +1,90 @@
+import joi from "joi";
+
+// register
+export const registerSchema = joi
+  .object({
+    firstName: joi.string().min(3).max(20).required(),
+    lastName: joi.string().min(3).max(20).required(),
+    email: joi
+      .string()
+      .email({
+        minDomainSegments: 2,
+        maxDomainSegments: 3,
+        tlds: { allow: ["com", "net", "edu", "org"] },
+      })
+      .message("Invalid Email")
+      .required(),
+    password: joi.string().required(),
+    cPassword: joi
+      .string()
+      .valid(joi.ref("password"))
+      .required(),
+    phoneNumber: joi
+      .string()
+      .regex(/^(\+201|01|00201)[0-2,5]{1}[0-9]{8}$/)
+      .message("Invalid phone number format!")
+      .required(),
+  })
+  .required();
+
+// activate account
+export const activateSchema = joi
+  .object({
+    activationCode: joi.string().required(),
+  })
+  .required();
+
+// login
+export const loginSchema = joi
+  .object({
+    email: joi
+      .string()
+      .email({
+        minDomainSegments: 2,
+        maxDomainSegments: 3,
+        tlds: { allow: ["com", "net", "edu", "org"] },
+      })
+      .required(),
+    password: joi.string().required(),
+  })
+  .required();
+
+// change password
+export const changePasswordSchema = joi
+  .object({
+    oldPassword: joi.string().required(),
+    newPassword: joi.string().required(),
+    cPassword: joi.string().valid(joi.ref("newPassword")).required(),
+  })
+  .required();
+
+// send forget code
+export const forgetCodeSchema = joi
+  .object({
+    email: joi
+      .string()
+      .email({
+        minDomainSegments: 2,
+        maxDomainSegments: 3,
+        tlds: { allow: ["com", "net", "edu", "org"] },
+      })
+      .required(),
+  })
+  .required();
+
+// reset password
+export const resetPasswordSchema = joi
+  .object({
+    email: joi
+      .string()
+      .email({
+        minDomainSegments: 2,
+        maxDomainSegments: 3,
+        tlds: { allow: ["com", "net", "edu", "org"] },
+      })
+      .required(),
+    forgetCode: joi.string().required(),
+    password: joi.string().required(),
+    cPassword: joi.string().valid(joi.ref("password")).required(),
+  })
+  .required();
